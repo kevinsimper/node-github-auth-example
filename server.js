492909235b63a5a926e1d56a1cc7d5cc5f3c2048
Kevin Simper <kevin.simper@gmail.com>
2020-03-26 22:44:33 +0100

    Use dotenv to load .env

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express'
 
 const app = express()
+
+const client_id = process.env.GITHUB_CLIENT_ID
+const client_secret = process.env.GITHUB_CLIENT_SECRET
+console.log({ client_id, client_secret })
+
 app.get('/', (req, res) => {
   res.send('Hello GitHub auth')
 })
