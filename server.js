07d7ef9f4a55f5068d2b36d6c3b525d9cb8f2923
Kevin Simper <kevin.simper@gmail.com>
2020-03-26 22:45:10 +0100

    Add github login redirect

--- server.js
+++ server.js
@@ -9,5 +9,15 @@ console.log({ client_id, client_secret })
 app.get('/', (req, res) => {
   res.send('Hello GitHub auth')
 })
+
+app.get('/login/github', (req, res) => {
+  const redirect_uri = 'http://localhost:9000/login/github/callback'
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${
+      process.env.GITHUB_CLIENT_ID
+    }&redirect_uri=${redirect_uri}`
+  )
+})
+
 const PORT = process.env.PORT || 9000
 app.listen(PORT, () => console.log('Listening on localhost:' + PORT))
